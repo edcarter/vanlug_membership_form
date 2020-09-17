e562cbe311200e4c3b3da2559c4c8f7bf2b7be09
Elias Carter <edcarter@ualberta.ca>
2020-09-17 11:20:16 -0700

    some small cleanup to make linter happy. fix some imports

--- cgi-bin/vl_man_mbrshp_form.url.py
+++ cgi-bin/vl_man_mbrshp_form.url.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+"""
+This CGI script is responsible for taking the new-member
+form data and inserting that data into a mysql table.
 
-# Here is a good example for python CGI scripts:
-# https://www.tutorialspoint.com/python/python_cgi_programming.htm
+Here are some good links to look at in order to understand this script:
+https://www.tutorialspoint.com/python/python_cgi_programming.htm
+https://docs.python.org/3.6/library/cgi.html
+https://docs.python.org/3.6/library/cgitb.html
+https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
+"""
+
 
 import cgi
 import cgitb
+import os
+
 import mysql.connector
 
 # Dump error logs to the same directory where this script lives
@@ -21,7 +31,7 @@ membership_db = mysql.connector.connect(
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
 id_obt = form.getvalue("id_obt", None)
-id_vlmbr_opt = form.getvalue("id_vlmbr_opt", None)
+id_vlmbr_obt = form.getvalue("id_vlmbr_obt", None)
 id_pmp = form.getvalue("id_pmp", None)
 id_pmp_usr = form.getvalue("id_pmp_usr", None)
 id_vfb = form.getvalue("id_vfb", None)
@@ -50,20 +60,22 @@ subtotal = form.getvalue("subtotal", None)
 total = form.getvalue("total", None)
 
 cursor = membership_db.cursor()
-query = ("INSERT INTO membership_obt "
+QUERY = ("INSERT INTO membership_obt "
          "(id_obt, id_vlmbr_obt, id_pmp, id_pmp_usr, id_vfb, id_vfb_usr, fname, lname,"
          "email, usrname, password, phone_num, addr1, addr2, city, prov_state, postalc_zip,"
          "comments_exprtise, start_date, expir_date, mbr_status, brd_posit, keypunch,"
          "paymt_type, amount, donation, subtotal, total) "
-         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, "
+         "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 params = (id_obt, id_vlmbr_obt, id_pmp, id_pmp_usr, id_vfb, id_vfb_usr, fname, lname,
           email, usrname, password, phone_num, addr1, addr2, city, prov_state, postalc_zip,
           comments_exprtise, start_date, expir_date, mbr_status, brd_posit, keypunch,
           paymt_type, amount, donation, subtotal, total)
 
-cursor.execute(query, params)
+cursor.execute(QUERY, params)
 membership_db.commit()
 membership_db.close()
 
 print("Content-type:text/plain\n")
 print("Status: 201 CREATED\n")
+print("Successfully added a member!\n")
